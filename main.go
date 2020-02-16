6ae7966f253a6ab7a0bfb885b0408ab5cd3aa2de
Sandro Santana <inbox.sandro@gmail.com>
2020-02-16 17:12:55 -0300

    menu and other instructions added in functions for more redability

--- main.go
+++ main.go
@@ -4,18 +4,41 @@ import "fmt"
 
 func main() {
 
+	showIntro()
+
+	showMenu()
+
+	command := captureCommand()
+
+	switch command {
+	case 1:
+		fmt.Println("Monitoring...")
+	case 2:
+		fmt.Println("Show logs...")
+	case 0:
+		fmt.Println("Exit program...")
+	}
+}
+
+func showIntro() {
 	version := 1.0
 	fmt.Println("Monitoring sites - version: ", version)
 	fmt.Println()
+}
+
+func showMenu() {
 	fmt.Println("Initiating monitoring sites program...")
 	fmt.Println("1- Initiating monitor")
 	fmt.Println("2- Show logs")
 	fmt.Println()
 	fmt.Println("0- Exit program")
 	fmt.Println()
+}
 
+func captureCommand() int {
 	command := 0
 	fmt.Println("What's your command to the program: ")
 	fmt.Scan(&command)
 	fmt.Println("The command that you choosed is", command)
+	return command
 }
